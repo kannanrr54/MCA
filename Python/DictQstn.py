5d76d08d5b7dd53eb9803a8ea923d451d8cec490
kannanrr54 <kannanrr54@gmail.com>
2021-11-09 09:46:56 -0800

    updates HW

--- Python/DictQstn.py
+++ Python/DictQstn.py
@@ -1,6 +1,9 @@
+import operator
 months={"Jan":30,"Feb":28,"Mar":31,"Apr":30,"May":31,"Jun":30,"Jul":31,"Aug":30,"Sep":31,"Oct":30,"Nov":31,"Dec":30}
 print("enter the month short form to get the total no:of days")
 s=input()
 print(s ,"has ",months[s], "Days")
-print("printing values in Aplbhabetical order")
-sr=sorted(months.items(),key=itemgetter(0))
+print("printing values in Aplbhabetical order of keys\n" ,sorted(months.items(),key=operator.itemgetter(0)))
+print("printing values in Ascending order of Months\n" ,sorted(months.items()))
+    
+
