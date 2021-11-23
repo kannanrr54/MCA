6c5a076b3244311f4ad899e070c231e1d4499ecd
Kannan <kannanrr54@gmail.com>
2021-11-23 15:35:16 +0530

    functions

---
+++ Python/SqrList/PrintSqrList.py
@@ -0,0 +1,5 @@
+import SqrList
+print("enter limit to get list")
+x=int(input())
+y=int(input())
+print(SqrList.SqrList(x,y))
---
+++ Python/SqrList/SqrList.py
@@ -0,0 +1,5 @@
+def SqrList(x,y):
+    a=[]
+    for i in range(x,y):
+        a.append(i*i)
+    return a
