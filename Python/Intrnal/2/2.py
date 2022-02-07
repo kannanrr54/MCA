24fcd2281688d2e5326219e895c4444ebd0c3519
kannanrr54 <kannanrr54@gmail.com>
2022-02-07 10:14:18 +0530

    update

--- Python/Intrnal/2/2.py
+++ Python/Intrnal/2/2.py
@@ -4,14 +4,20 @@ print("")
 for i in f.readlines():
         print(i.strip())
 f.close()
+f=open("input.txt","r")
+o=open("output.txt","w")
+for c in f.readlines():
+    if(len(c.split(" "))>5 and c[0].isupper()):
+        o.write(c)
+f.close()
+o.close()
 print("")
 print("Output--------------------------------")
 print("")
-f=open("input.txt","r")
-for c in f.readlines():
-    if(len(c.split(" "))>5 and c[0].isupper()):
-        print(c.strip())
-f.close()
+o=open("output.txt","r")
+for c in o.readlines():
+    print(c.strip())
+o.close()
 
 
 
