7637b66c2bc70da1dd4dfa8e2315f2c86d5173fd
kannanrr54 <kannanrr54@gmail.com>
2022-03-01 22:09:00 +0530

    nf

--- Web/CO3/FormValPhp.php
+++ Web/CO3/FormValPhp.php
@@ -4,53 +4,97 @@
 <body>
     <center>
         <div style="border: 1px solid black;width:40%;padding:20px">
-            <table>
-                    <tr>
-                        <td>Enter Name : </td>
-                        <td><input type="text" placeholder="Name" id="name" required=""/></td>
-                    </tr>
-                    <tr>
-                        <td><p>Mobile Number    :</p></td>
-                        <td><input type="number" placeholder="number"  required="" id="num"/></td>
-                    </tr>
-                    <tr>
-                        <td><p>Email   :</p></td>
-                        <td><input type="text" placeholder="Email"  required="" id="email"/></td>
-                    </tr>
-                    <tr>
-                        <td><p>Date Of Birth     :</p></td>
-                        <td><input type="date" class="input" id="dob" required=""></td>
-                    </tr>
-                    <tr>
-                        <td><input type="text" placeholder="Username" required="" name="user"></td>
-                        <td><input type="text" placeholder="Password" required="" name="pass"></td>
-                    </tr>
-                    <tr>
-                        <td colspan="2"><input type="submit" name="s"></center></td>
-                    </tr>
-            </table>
-            <div>
-            <div style="visibility: hidden;width: 50px;color :white;background-color: green;" id="p">No Errors</div>
-    </center>
+        <form method="POST">
+        <table>
+						<tr>
+							<td>Enter Name : </td>
+							<td>
+								<input type="text" placeholder="Name" name="user" />
+							</td>
+						</tr>
+						<tr>
+							<td>
+								<p>Mobile Number :</p>
+							</td>
+							<td>
+								<input type="text" placeholder="number" name="num" />
+							</td>
+						</tr>
+						<tr>
+							<td>
+								<p>Email :</p>
+							</td>
+							<td>
+								<input type="text" placeholder="Email" name="email" />
+							</td>
+						</tr>
+						<tr>
+							<td>
+								<input type="text" placeholder="Username" name="user">
+							</td>
+							<td>
+								<input type="text" placeholder="Password" name="pass">
+							</td>
+						</tr>
+						<tr>
+							<td colspan="2">
+								<center>
+									<input type="submit" name="s">
+								</center>
+							</td>
+						</tr>
+					</table>
+</form>
+    
         <?php
         if(isset($_POST["s"]))
         {
-            $C1=preg_match("/[a-z]/",$_POST["pass"]);
-            $C2=preg_match("/[A-Z]/",$_POST["pass"]);
-            $C3=preg_match("/[0-9]/",$_POST["pass"]);
-            if(!($C1 && $C2 && $C3) && !strlen($_POST["pass"])<8)
+            $con=mysqli_connect("localhost","root","","MCA");
+            $error=0;
+            $name=$_POST["user"];
+            $username=$_POST["user"];
+            $pass=$_POST["pass"];
+            $num=$_POST["num"];
+            $email=$_POST["email"];
+            $C1=preg_match("/[a-z]/",$pass);
+            $C2=preg_match("/[A-Z]/",$pass);
+            $C3=preg_match("/[0-9]/",$pass);
+            $C5=preg_match("/[0-9]/",$num);
+            $C4=preg_match("/^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3})+$/",$email);
+            
+            if(!($C1 && $C2 && $C3) && !strlen($pass)<8)
             {
                 echo "Password not strong";
+                $error=1;
             }
-            ?><br><?php
-            if($_POST["user"]=="")
+            ?>
+					<br>
+					<?php
+            if(!($C5) && $num!="")
             {
-                echo "Enter Username";
+                echo "Enter number only in Number field";
+                $error=1;
             }
-            ?><br><?php
-            if($_POST["pass"]=="")
+            if(strlen($num)!=10)
             {
-                echo "Enter Password";
+                echo "Movbile number can only be 10 digits";
+                $error=1;
+            }
+            ?>
+						<br>
+						<?php
+            if(!($C4) && $email!="")
+            {
+                echo "Enter email in correct format";
+                $error=1;
+            }
+            ?>
+							<br>
+							<?php
+            if($username=="" || $pass=="" || $num=="" || $email=="" || $name=="")
+            {
+                echo "Enter all data";
+                $error=1;
             }
         }
     ?>
