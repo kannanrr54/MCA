a512cf001ed7f53ea96d712e16a011b8e40c04f1
SREELAKSHMI.S <sreekollam1997@gmail.com>
2022-03-21 10:33:07 +0530

    ffds

--- Java/Cycle1/src/T1.java
+++ Java/Cycle1/src/T1.java
@@ -1,4 +1,14 @@
-
-public class T1 {
-
+import java.util.*;
+class product {
+    public static void main(String[] args) {
+        System.out.println("Enter product details\nEnter product name");
+        Scanner s=new Scanner(System.in);
+        String name=s.nextLine();
+        System.out.println("Enter product type");
+        String type=s.nextLine();
+        System.out.println("Enter product price");
+        String price=s.nextLine();
+        System.out.println("-----------------------------------\n");
+        System.out.println("Name : "+name+"\nType : "+type+"\nPrice : "+price);
+    }
 }
