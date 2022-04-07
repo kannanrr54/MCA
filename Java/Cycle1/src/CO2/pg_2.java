6c577ec3c1f87f892fde5d36b8cd46110e661728
jeslin <sariga947@gmail.com>
2022-04-07 16:15:06 +0530

    fgdfg

---
+++ Java/Cycle1/src/CO2/pg_2.java
@@ -0,0 +1,11 @@
+package CO2;
+import java.util.*;
+public class pg_2 {
+public static void main(String [] args){
+	int[] a=new int[10];
+	Scanner s=new Scanner(System.in);
+	for (int i=0;i<3;i++){
+		a[i]=s.nextInt();
+	}
+}
+}
