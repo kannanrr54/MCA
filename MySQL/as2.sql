147e94bfdadcd4f4c4a3eb34c67a8eefee8b0bd1
jeslin <sariga947@gmail.com>
2022-04-11 16:13:45 +0530

    Create as2.sql

---
+++ MySQL/as2.sql
@@ -0,0 +1,14 @@
+use as2;
+CREATE TABLE T2 ( a INTEGER, b CHAR(10));
+
+call insertVariable(1,"A");
+SET SQL_SAFE_UPDATES = 0;
+delete from T2;
+select * from t2;
+create table employee(
+id int,
+name varchar(20),
+Basic double,
+HRA double,
+DA double);
+insert into employee values(100,"a",10000,1500,1200),(101,"a",12000,1500,1200);
