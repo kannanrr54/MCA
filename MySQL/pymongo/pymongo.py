4a1986e95e55b5f6f948f35e8fcab8874dcaddd8
KannanRR <kannanrr54@gmail.com>
2022-06-08 14:27:10 +0530

    Update pymongo.py

--- MySQL/pymongo/pymongo.py
+++ MySQL/pymongo/pymongo.py
@@ -1,4 +1,5 @@
 from pymongo import MongoClient
+#mongodb+srv://<username>:<pass>@cluster0.hxeu7bl.mongodb.net/?retryWrites=true&w=majority
 client=MongoClient("mongodb+srv://jeslin:jes@cluster0.hxeu7bl.mongodb.net/?retryWrites=true&w=majority")
 db=client.gettingStarted
 people=db.people
