2eb153f1fe3fb0a35f58d15624276b4c057b1d11
KannanRR <kannanrr54@gmail.com>
2022-06-10 12:14:21 +0530

    gfhgfh

---
+++ Java/Cycle1/src/CO4/pg_6.java
@@ -0,0 +1,20 @@
+package CO4;
+class Fib implements Runnable{
+	public void run(){
+		for (int i=0;i<12;i++){
+			
+		}
+	}
+}
+class Ev implements Runnable{
+	public void run()
+	{
+		
+	}
+}
+public class pg_6 {
+	public static void main(String [] args){
+		Fib f=new Fib();
+		Ev e=new Ev();
+	}
+}
